0d6cba0e379de8d3bd1c053d3a46084e95f6290d
iansun <lauer3912@gmail.com>
2016-05-06 17:25:32 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"ExifImage",
+        appID:"com.romanysoft.app.macos.ExifImage",
+        documentTitle:"ExifImage By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-76676167-10",
+        gitHome:"//github.com/Romanysoft/ExifImage",
+        reportIssueUrl:"//github.com/Romanysoft/ExifImage/issues",
+        changeLogUrl:"//github.com/Romanysoft/ExifImage/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/ExifImage/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
